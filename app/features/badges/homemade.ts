2bc4a5471df992149d026c4f0bc164a1b047c801
benny B franklin <lovebailey74@outlook.com>
2025-01-18 02:22:43 +1100

    hi

--- app/features/badges/homemade.ts
+++ app/features/badges/homemade.ts
@@ -15,6 +15,11 @@ export const homemadeBadges: BadgeInfo[] = [
 	// 	authorDiscordId: "123456789012345678",
 	// },
 	{
+		displayName: "Token Of Appreciation",
+		fileName: "kittybukcet",
+		authorDiscordId: "1170249805373657093",
+	},
+	{
 		displayName: "Tough Tides",
 		fileName: "wave1234567_4",
 		authorDiscordId: "1170249805373657093",
